28cb9e069507e3d4483b1bae8beb4ce08de33a44
RoshiniAccenture <r.ad.ravichandran@accenture.com>
2026-01-28 05:57:40 +0000

    Update API URLs to use automatic-doodle-q7r7gw5xvr9vf46xr and set ALLOWED_HOSTS for Codespaces and localhost

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,14 @@ SECRET_KEY = 'django-insecure-@g#s!^!b#2zuoludppv=th)qk9h_^adqhq7vzxqq5##lvy1)k)
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+
+# Allow codespace URL and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
